fc50e46e083f86ced8f7d96feb548fe26eb7c952
Skaruts <Skaruts@users.noreply.github.com>
2023-11-09 18:58:15 +0000

    add config file

--- src/tb_loader.cpp
+++ src/tb_loader.cpp
@@ -2,17 +2,21 @@
 
 #include <godot_cpp/core/class_db.hpp>
 #include <godot_cpp/variant/utility_functions.hpp>
+#include <godot_cpp/classes/config_file.hpp>
 
 #include <builder.h>
 
 void TBLoader::_bind_methods() {
+	ClassDB::bind_method(D_METHOD("set_create_config", "create_config"), &TBLoader::set_create_config);
+	ClassDB::bind_method(D_METHOD("get_create_config"), &TBLoader::get_create_config);
+
 	ClassDB::bind_method(D_METHOD("set_map", "map_resource"), &TBLoader::set_map);
 	ClassDB::bind_method(D_METHOD("get_map"), &TBLoader::get_map);
-	ClassDB::bind_method(D_METHOD("set_inverse_scale", "map_inverse_scale"), &TBLoader::set_inverse_scale);
+	ClassDB::bind_method(D_METHOD("set_inverse_scale", "inverse_scale"), &TBLoader::set_inverse_scale);
 	ClassDB::bind_method(D_METHOD("get_inverse_scale"), &TBLoader::get_inverse_scale);
 
-	ClassDB::bind_method(D_METHOD("set_lighting_unwrap_uv2", "lighting_unwrap_uv2"), &TBLoader::set_lighting_unwrap_uv2);
-	ClassDB::bind_method(D_METHOD("get_lighting_unwrap_uv2"), &TBLoader::get_lighting_unwrap_uv2);
+	ClassDB::bind_method(D_METHOD("set_unwrap_uv2", "unwrap_uv2"), &TBLoader::set_unwrap_uv2);
+	ClassDB::bind_method(D_METHOD("get_unwrap_uv2"), &TBLoader::get_unwrap_uv2);
 
 	ClassDB::bind_method(D_METHOD("set_collision", "option_collision"), &TBLoader::set_collision);
 	ClassDB::bind_method(D_METHOD("get_collision"), &TBLoader::get_collision);
@@ -27,46 +31,108 @@ void TBLoader::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("set_visual_layer_mask", "option_visual_layer_mask"), &TBLoader::set_visual_layer_mask);
 	ClassDB::bind_method(D_METHOD("get_visual_layer_mask"), &TBLoader::get_visual_layer_mask);
 
-	ClassDB::bind_method(D_METHOD("set_entity_common", "entity_common"), &TBLoader::set_entity_common);
-	ClassDB::bind_method(D_METHOD("get_entity_common"), &TBLoader::get_entity_common);
 	ClassDB::bind_method(D_METHOD("set_entity_path", "entity_path"), &TBLoader::set_entity_path);
 	ClassDB::bind_method(D_METHOD("get_entity_path"), &TBLoader::get_entity_path);
 	ClassDB::bind_method(D_METHOD("set_texture_path", "texture_path"), &TBLoader::set_texture_path);
 	ClassDB::bind_method(D_METHOD("get_texture_path"), &TBLoader::get_texture_path);
 
-	ClassDB::bind_method(D_METHOD("set_post_build_script_path", "post_build_script_path"), &TBLoader::set_post_build_script_path);
-	ClassDB::bind_method(D_METHOD("get_post_build_script_path"), &TBLoader::get_post_build_script_path);
+	ClassDB::bind_method(D_METHOD("set_build_script_path", "build_script_path"), &TBLoader::set_build_script_path);
+	ClassDB::bind_method(D_METHOD("get_build_script_path"), &TBLoader::get_build_script_path);
 
 
 	ClassDB::bind_method(D_METHOD("clear"), &TBLoader::clear);
 	ClassDB::bind_method(D_METHOD("build_meshes"), &TBLoader::build_meshes);
 
+
+
+	ADD_PROPERTY(PropertyInfo(Variant::BOOL,   "create_config",  PROPERTY_HINT_NONE),             "set_create_config",        "get_create_config");
+
 	ADD_GROUP("Map", "map_");
-	ADD_PROPERTY(PropertyInfo(Variant::STRING, "map_resource", PROPERTY_HINT_FILE, "*.map"), "set_map", "get_map");
-	ADD_PROPERTY(PropertyInfo(Variant::INT, "map_inverse_scale", PROPERTY_HINT_NONE, "Inverse Scale"), "set_inverse_scale", "get_inverse_scale");
-	ADD_PROPERTY(PropertyInfo(Variant::STRING, "map_post_build_script_path", PROPERTY_HINT_FILE, "*.gd"), "set_post_build_script_path", "get_post_build_script_path");
+	ADD_PROPERTY(PropertyInfo(Variant::STRING, "file_path",  PROPERTY_HINT_FILE, "*.map"),    "set_map",                  "get_map");
 
-	ADD_GROUP("Lighting", "lighting_");
-	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "lighting_unwrap_uv2", PROPERTY_HINT_NONE, "Unwrap UV2"), "set_lighting_unwrap_uv2", "get_lighting_unwrap_uv2");
+	ADD_GROUP("Options", "");
+	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "unwrap_uv2",        PROPERTY_HINT_NONE),             "set_unwrap_uv2",          "get_unwrap_uv2");
+	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "world_collisions",  PROPERTY_HINT_NONE),             "set_collision",           "get_collision");
+	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "filter_nearest",    PROPERTY_HINT_NONE),             "set_filter_nearest",      "get_filter_nearest");
+	ADD_PROPERTY(PropertyInfo(Variant::INT,  "visual_layer_mask", PROPERTY_HINT_LAYERS_3D_RENDER), "set_visual_layer_mask",   "get_visual_layer_mask");
 
-	ADD_GROUP("Options", "option_");
-	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "option_collision", PROPERTY_HINT_NONE, "Collision"), "set_collision", "get_collision");
-	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "option_skip_hidden_layers", PROPERTY_HINT_NONE, "Skip Hidden Layers"), "set_skip_hidden_layers", "get_skip_hidden_layers");
-	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "option_filter_nearest", PROPERTY_HINT_NONE, "Texture Filter Nearest"), "set_filter_nearest", "get_filter_nearest");
-	ADD_PROPERTY(PropertyInfo(Variant::STRING, "option_clip_texture_name", PROPERTY_HINT_NONE, "Clip Texture"), "set_clip_texture_name", "get_clip_texture_name");
-	ADD_PROPERTY(PropertyInfo(Variant::STRING, "option_skip_texture_name", PROPERTY_HINT_NONE, "skip Texture"), "set_skip_texture_name", "get_skip_texture_name");
-	ADD_PROPERTY(PropertyInfo(Variant::INT, "option_visual_layer_mask", PROPERTY_HINT_LAYERS_3D_RENDER), "set_visual_layer_mask", "get_visual_layer_mask");
-
-	ADD_GROUP("Entities", "entity_");
-	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "entity_common", PROPERTY_HINT_NONE, "Common Entities"), "set_entity_common", "get_entity_common");
-	ADD_PROPERTY(PropertyInfo(Variant::STRING, "entity_path", PROPERTY_HINT_DIR, "Entity Path"), "set_entity_path", "get_entity_path");
-
-	ADD_GROUP("Textures", "texture_");
-	ADD_PROPERTY(PropertyInfo(Variant::STRING, "texture_path", PROPERTY_HINT_DIR, "Textures Path"), "set_texture_path", "get_texture_path");
 }
 
 TBLoader::TBLoader() {}
 TBLoader::~TBLoader() {}
+
+
+void TBLoader::clear() {
+	while (get_child_count() > 0) {
+		auto child = get_child(0);
+		remove_child(child);
+		child->queue_free();
+	}
+}
+
+void TBLoader::build_meshes() {
+	clear();
+
+	if (load_config()) load_settings();
+
+	Builder builder(this);
+	builder.load_map(m_map_path);
+	builder.build_map();
+}
+
+void TBLoader::_ready() {
+	m_config = (Ref<ConfigFile>)memnew(ConfigFile());
+
+	if (!load_config())
+		UtilityFunctions::printerr("TBLoader config file doesn't exist. Click 'Create Config' in the TBLoader node to create one.");
+}
+
+void TBLoader::create_config() {
+	// if it already exists, load it first, so it doesn't get overwritten
+	if (load_config()) load_settings();
+
+	m_config->set_value("map",      "inverse_scale",      m_inverse_scale);
+	m_config->set_value("map",      "build_script",       m_build_script_path);
+
+	m_config->set_value("options",  "skip_hidden_layers", m_skip_hidden_layers);
+	// m_config->set_value("options",  "clip_texture_name",  m_clip_texture_name);
+	m_config->set_value("options",  "skip_texture_name",  m_skip_texture_name);
+
+	m_config->set_value("entities", "entity_path",        m_entity_path);
+	m_config->set_value("assets",   "texture_path",       m_texture_path);
+
+	save_config();
+}
+
+bool TBLoader::load_config() {
+	return m_config->load(m_cfg_path) == OK;
+}
+
+void TBLoader::save_config() {
+	m_config->save(m_cfg_path);
+}
+
+void TBLoader::load_settings() {
+	m_inverse_scale          = m_config->get_value("map",      "inverse_scale",      m_inverse_scale);
+	m_build_script_path      = m_config->get_value("map",      "build_script",       m_build_script_path);
+
+	m_skip_hidden_layers     = m_config->get_value("options",  "skip_hidden_layers", m_skip_hidden_layers);
+	// m_clip_texture_name      = m_config->get_value("options",  "clip_texture_name",  m_clip_texture_name);
+	m_skip_texture_name      = m_config->get_value("options",  "skip_texture_name",  m_skip_texture_name);
+
+	m_entity_path            = m_config->get_value("entities", "entity_path",        m_entity_path);
+	m_texture_path           = m_config->get_value("assets",   "texture_path",       m_texture_path);
+}
+
+
+void TBLoader::set_create_config(bool enable) {
+	create_config();
+}
+
+bool TBLoader::get_create_config() {
+	return false;
+}
+
 
 void TBLoader::set_map(const String& map) {
 	m_map_path = map;
@@ -76,6 +142,7 @@ String TBLoader::get_map() const {
 	return m_map_path;
 }
 
+
 void TBLoader::set_inverse_scale(int scale) {
 	m_inverse_scale = scale;
 }
@@ -84,13 +151,15 @@ int TBLoader::get_inverse_scale() {
 	return m_inverse_scale;
 }
 
-void TBLoader::set_lighting_unwrap_uv2(bool enabled) {
-	m_lighting_unwrap_uv2 = enabled;
+
+void TBLoader::set_unwrap_uv2(bool enabled) {
+	m_unwrap_uv2 = enabled;
 }
 
-bool TBLoader::get_lighting_unwrap_uv2() {
-	return m_lighting_unwrap_uv2;
+bool TBLoader::get_unwrap_uv2() {
+	return m_unwrap_uv2;
 }
+
 
 void TBLoader::set_collision(bool enabled) {
 	m_collision = enabled;
@@ -100,6 +169,7 @@ bool TBLoader::get_collision() {
 	return m_collision;
 }
 
+
 void TBLoader::set_skip_hidden_layers(bool enabled) {
 	m_skip_hidden_layers = enabled;
 }
@@ -107,6 +177,7 @@ void TBLoader::set_skip_hidden_layers(bool enabled) {
 bool TBLoader::get_skip_hidden_layers() {
 	return m_skip_hidden_layers;
 }
+
 
 void TBLoader::set_filter_nearest(bool enabled) {
 	m_filter_nearest = enabled;
@@ -116,6 +187,7 @@ bool TBLoader::get_filter_nearest() {
 	return m_filter_nearest;
 }
 
+
 void TBLoader::set_clip_texture_name(const String& clip_texture_name) {
 	m_clip_texture_name = clip_texture_name;
 }
@@ -123,6 +195,7 @@ void TBLoader::set_clip_texture_name(const String& clip_texture_name) {
 String TBLoader::get_clip_texture_name() {
 	return m_clip_texture_name;
 }
+
 
 void TBLoader::set_skip_texture_name(const String& skip_texture_name) {
 	m_skip_texture_name = skip_texture_name;
@@ -132,6 +205,7 @@ String TBLoader::get_skip_texture_name() {
 	return m_skip_texture_name;
 }
 
+
 uint32_t TBLoader::get_visual_layer_mask() {
 	return m_visual_layer_mask;
 }
@@ -140,13 +214,6 @@ void TBLoader::set_visual_layer_mask(uint32_t visual_layer_mask) {
 	m_visual_layer_mask = visual_layer_mask;
 }
 
-void TBLoader::set_entity_common(bool enabled) {
-	m_entity_common = enabled;
-}
-
-bool TBLoader::get_entity_common() {
-	return m_entity_common;
-}
 
 void TBLoader::set_entity_path(const String& path) {
 	m_entity_path = path;
@@ -155,6 +222,7 @@ void TBLoader::set_entity_path(const String& path) {
 String TBLoader::get_entity_path() {
 	return m_entity_path;
 }
+
 
 void TBLoader::set_texture_path(const String& path) {
 	if (path.is_empty()) {
@@ -169,27 +237,11 @@ String TBLoader::get_texture_path() {
 
 
 
-void TBLoader::set_post_build_script_path(const String& path) {
-	m_post_build_script_path = path;
+void TBLoader::set_build_script_path(const String& path) {
+	m_build_script_path = path;
 }
 
-String TBLoader::get_post_build_script_path() {
-	return m_post_build_script_path;
+String TBLoader::get_build_script_path() {
+	return m_build_script_path;
 }
 
-
-void TBLoader::clear() {
-	while (get_child_count() > 0) {
-		auto child = get_child(0);
-		remove_child(child);
-		child->queue_free();
-	}
-}
-
-void TBLoader::build_meshes() {
-	clear();
-
-	Builder builder(this);
-	builder.load_map(m_map_path);
-	builder.build_map();
-}
--- src/tb_loader.h
+++ src/tb_loader.h
@@ -7,72 +7,99 @@
 
 #include <godot_cpp/classes/node3d.hpp>
 #include <godot_cpp/classes/control.hpp>
+#include <godot_cpp/classes/config_file.hpp>
 
 using namespace godot;
 
 class TBLoader : public Node3D {
 	GDCLASS(TBLoader, Node3D);
 
+
+private:
+	String    m_cfg_path                       = "res://tbloader_config.cfg";
+
 public:
-	String m_map_path;
-	int m_inverse_scale = 16;
+	Ref<ConfigFile> m_config;
 
-	bool m_lighting_unwrap_uv2 = false;
+	int       m_inverse_scale          = 64;
+	bool      m_skip_hidden_layers     = true;
+	String    m_entity_path            = "res://scenes/entities";
+	String    m_texture_path           = "res://assets/textures";
+	String    m_build_script_path      = "";
+	String    m_clip_texture_name      = "";
+	String    m_skip_texture_name      = "";
 
-	bool m_collision = true;
-	bool m_skip_hidden_layers = true;
-	bool m_filter_nearest = false;
+	String    m_map_path               = "";
+	bool      m_unwrap_uv2             = false;    // global ???
+	bool      m_collision              = true;
+	bool      m_filter_nearest         = false;
+	uint32_t  m_visual_layer_mask      = 1;
 
-	bool m_entity_common = true;
-	String m_entity_path = "res://scenes/entities";
-	String m_texture_path = "res://assets/textures";
-	String m_post_build_script_path = "res://scripts/gmi_post_build.gd";
-	String m_clip_texture_name = "";
-	String m_skip_texture_name = "";
-	uint32_t m_visual_layer_mask = 1;
 
 protected:
 	static void _bind_methods();
+
+
+private:
+	bool load_config();
+	void save_config();
+	void create_config();
+
+	void load_settings();
+
 
 public:
 	TBLoader();
 	~TBLoader();
 
+	void TBLoader::_ready();
+
+	void clear();
+	void build_meshes();
+
+	void set_create_config(bool enable);
+	bool get_create_config();
+
+	void set_build_script_path(const String& path);
+	String get_build_script_path();
+
+
 	// Map
 	void set_map(const String& map);
 	String get_map() const;
+
+	// Options
+	void set_unwrap_uv2(bool enabled);
+	bool get_unwrap_uv2();
+
+	void set_collision(bool enabled);
+	bool get_collision();
+
+	void set_filter_nearest(bool enabled);
+	bool get_filter_nearest();
+
+	uint32_t get_visual_layer_mask();
+	void set_visual_layer_mask(uint32_t visual_layer_mask);
+
+
+
+
 	void set_inverse_scale(int scale);
 	int get_inverse_scale();
 
-	// Lighting
-	void set_lighting_unwrap_uv2(bool enabled);
-	bool get_lighting_unwrap_uv2();
-
-	// Options
-	void set_collision(bool enabled);
-	bool get_collision();
 	void set_skip_hidden_layers(bool enabled);
 	bool get_skip_hidden_layers();
-	void set_filter_nearest(bool enabled);
-	bool get_filter_nearest();
+
 	void set_clip_texture_name(const String& clip_texture);
 	String get_clip_texture_name();
 	void set_skip_texture_name(const String& skip_texture);
 	String get_skip_texture_name();
-	uint32_t get_visual_layer_mask();
-	void set_visual_layer_mask(uint32_t visual_layer_mask);
 
-	// Entities
-	void set_entity_common(bool enabled);
-	bool get_entity_common();
 	void set_entity_path(const String& path);
 	String get_entity_path();
+
 	void set_texture_path(const String& path);
 	String get_texture_path();
 
-	void set_post_build_script_path(const String& path);
-	String get_post_build_script_path();
 
-	void clear();
-	void build_meshes();
 };
